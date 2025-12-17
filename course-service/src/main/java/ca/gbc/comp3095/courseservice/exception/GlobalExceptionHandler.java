63e48a26ec96c8f6c64f47a621fa125871c8371c
James Jones <jjsfitnesstoronto@gmail.com>
2025-12-17 00:08:52 -0500

    Use @RestControllerAdvice for consistent JSON error responses

--- course-service/src/main/java/ca/gbc/comp3095/courseservice/exception/GlobalExceptionHandler.java
+++ course-service/src/main/java/ca/gbc/comp3095/courseservice/exception/GlobalExceptionHandler.java
@@ -10,13 +10,13 @@ package ca.gbc.comp3095.courseservice.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.HashMap;
 import java.util.Map;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(CourseNotFoundException.class)
