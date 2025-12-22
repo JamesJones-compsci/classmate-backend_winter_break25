a423fc43591d99309db638cc168b3995e99e278e
James Jones <jjsfitnesstoronto@gmail.com>
2025-12-22 10:16:44 -0500

    Day 8: Document microservice architecture and add API Gateway skeleton

---
+++ api-gateway/src/main/java/ca/gbc/comp3095/apigateway/ApiGatewayApplication.java
@@ -0,0 +1,17 @@
+package ca.gbc.comp3095.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+
+
+		SpringApplication.run(ApiGatewayApplication.class, args);
+
+		System.out.println("API Gateway skeleton initialized (Day 8 placeholder).");
+	}
+
+}
