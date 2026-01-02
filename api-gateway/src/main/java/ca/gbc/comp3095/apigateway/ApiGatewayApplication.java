18d2179fdc90507722fbec6aaaaea5abd8f64cc8
James Jones <jjsfitnesstoronto@gmail.com>
2026-01-02 02:52:54 -0500

    Debugging the api-gateway access provided to Swagger documentation for each microservice

--- api-gateway/src/main/java/ca/gbc/comp3095/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/ca/gbc/comp3095/apigateway/ApiGatewayApplication.java
@@ -28,9 +28,26 @@ public class ApiGatewayApplication {
 				.route("course-service", r -> r.path("/api/v1/courses/**")
 						.uri("http://course-service:8080"))
 
+				// Course Service Swagger
+				.route("course-service-swagger", r -> r
+						.path("/course-service/v3/api-docs")
+						.filters(f -> f.rewritePath(
+								"/course-service/v3/api-docs",
+								"/v3/api-docs"))
+						.uri("http://course-service:8080"))
+
 
 				// Route /api/v1/grades/** to grade-service
 				.route("grade-service", r -> r.path("/api/v1/grades/**")
+						.uri("http://grade-service:8081"))
+
+
+				// Grade Service Swagger
+				.route("grade-service-swagger", r -> r
+						.path("/grade-service/v3/api-docs")
+						.filters(f -> f.rewritePath(
+								"/grade-service/v3/api-docs",
+								"/v3/api-docs"))
 						.uri("http://grade-service:8081"))
 
 
@@ -38,9 +55,25 @@ public class ApiGatewayApplication {
 				.route("task-service", r -> r.path("/api/v1/tasks/**")
 						.uri("http://task-service:8083"))
 
+				// Task Service Swagger
+				.route("task-service-swagger", r -> r
+						.path("/task-service/v3/api-docs")
+						.filters(f -> f.rewritePath(
+								"/task-service/v3/api-docs",
+								"/v3/api-docs"))
+						.uri("http://task-service:8083"))
+
 
 				// Route /api/v1/reminders/** to reminder-service
 				.route("reminder-service", r -> r.path("/api/v1/reminders/**")
+						.uri("http://reminder-service:8082"))
+
+				// Reminder Service Swagger
+				.route("reminder-service-swagger", r -> r
+						.path("/reminder-service/v3/api-docs")
+						.filters(f -> f.rewritePath(
+								"/reminder-service/v3/api-docs",
+								"/v3/api-docs"))
 						.uri("http://reminder-service:8082"))
 
 				.build();
